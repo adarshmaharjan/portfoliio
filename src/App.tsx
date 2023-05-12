7ab3a95d5f3236e53fd987f22c0f55d1939b3958
adarsh maharjan <adarsh986030@gmail.com>
2023-05-12 18:59:01 +0545

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+// const App: React.FC = () => {
+//   return <div>App</div>;
+// };
+function App(): ReturnType<React.FC> {
+  return <div>App</div>;
+}
+export default App;
