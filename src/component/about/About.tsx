f9b240efd94f86f1fed5e5badc64fda283d4954f
Adarsh Maharjan <40669144+adarshmaharjan@users.noreply.github.com>
2023-06-25 09:08:01 +0000

    [changed] the resume link

--- src/component/about/About.tsx
+++ src/component/about/About.tsx
@@ -1,21 +1,26 @@
-import { FC } from 'react';
-import { profileImg } from '../../common/images';
+import { FC } from "react";
+import { profileImg } from "../../common/images";
 
 const About: FC = () => {
   return (
     <section id="about" title="about">
       <div className="hero min-h-[calc(100vh-64px)] bg-base-300">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <img alt="profile" src={profileImg} className="max-w-full sm:max-w-sm rounded-lg shadow-2xl" />
+          <img
+            alt="profile"
+            src={profileImg}
+            className="max-w-full sm:max-w-sm rounded-lg shadow-2xl"
+          />
           <div>
             <h1 className="text-5xl font-header font-bold">About</h1>
             <p className="py-6 prose prose-slate">
-              I am a Software engineer learning and experiencing the world of coding and programming. I am committed to
-              utilizing my skills to further the company's mission and am proficient in various platforms, languages,
-              and embedded systems.
+              I am a Software engineer learning and experiencing the world of
+              coding and programming. I am committed to utilizing my skills to
+              further the company's mission and am proficient in various
+              platforms, languages, and embedded systems.
             </p>
             <a
-              href="https://drive.google.com/file/d/1nUI-tMK35MrgV8m0L2EHR_gWmxWxPOPe/view"
+              href="https://docs.google.com/document/d/1VtHpqIehQ52vIxqj7-h0kYzW9jr1zUvPXpoAlznl9DE/edit?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
             >
